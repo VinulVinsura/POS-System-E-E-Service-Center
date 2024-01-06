8890f7eec55ed237636715e21fccdb1325675fdb
Vinul Vinsura <vinulviksura16@gmail.com>
2024-01-06 22:33:49 +0530

    Create Main DashBoardForm

--- src/main/java/controller/DashBoardFormController.java
+++ src/main/java/controller/DashBoardFormController.java
@@ -1,6 +1,19 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class DashBoardFormController {
 
 
+    public TextField emailTex;
+    public TextField passwordTex;
+
+    public void adminButtonOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void emplyeeButtonOnAction(ActionEvent actionEvent) {
+
+    }
 }
